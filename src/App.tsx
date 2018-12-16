cf75dbfa3ec2efcf8c5dbe95a3cbb7c2be15da5b
baco16g <baco.16g@gmail.com>
2018-12-16 18:43:30 +0900

    feat(*): remove unnecessary files and initialize a project

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import React, { Component } from 'react'
-import './App.css'
-import logo from './logo.svg'
+import React from 'react'
+import Curousel from './Curousel'
 
-class App extends Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
-      </div>
-    )
-  }
+export default function App() {
+  return (
+    <>
+      <h1>Curousel with React Hooks</h1>
+      <Curousel />
+    </>
+  )
 }
-
-export default App
---
+++ src/Curousel/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Curousel() {
+  return <div>This is Curousel!</div>
+}
