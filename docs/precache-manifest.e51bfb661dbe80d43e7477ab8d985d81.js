f3b94a7a271c551be444fb0b5a91ef41f661221e
baco16g <baco.16g@gmail.com>
2018-12-18 20:46:10 +0900

    chore(*): add a docs folder for Github Pages

---
+++ docs/precache-manifest.e51bfb661dbe80d43e7477ab8d985d81.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4d1078c4b9c21060b9f6",
+    "url": "/static/js/main.4d1078c4.chunk.js"
+  },
+  {
+    "revision": "fc64a8d18d1ffa6a4630",
+    "url": "/static/js/1.fc64a8d1.chunk.js"
+  },
+  {
+    "revision": "06bc8fe9452d65e1c260b2b10dc43612",
+    "url": "/index.html"
+  }
+];
